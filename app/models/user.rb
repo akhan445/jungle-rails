5984d4fb202a98325cfcc2e3a86ee10fd7430526
Abdullah S. Khan <abdullahshamskhan@gmail.com>
2022-01-17 18:06:11 -0500

    Add class method to authenticate user on login

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,14 @@ class User < ActiveRecord::Base
   validates :password, confirmation: true, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
 
+  def self.authenticate_with_credentials(email, password)
+    user = self.find_by_email(email)
+
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
+
 end
